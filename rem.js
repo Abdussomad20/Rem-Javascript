2fbf59ff25d49ca9004258e0e43b510e99264fc9
Abdussomad20 <harzkid2018@gmail.com>
2023-01-15 16:04:09 +0100

    remainder

---
+++ rem.js
@@ -0,0 +1,12 @@
+const dog = {
+    name: 'Baba',
+    leg: 'smaall',
+    color: 'Yellow',
+    age: '23',
+    bark: 'woof woof',
+}
+console.log(dog.bark)
+console.log(dog.name)
+console.log(dog.leg)
+console.log(dog.color)
+console.log(dog.age)
